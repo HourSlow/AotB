b0e8901179142412aedd7d13ff99e16d019f81f8
Hour <andrija.pusic2009@gmail.com>
2026-04-05 19:10:26 +0200

    Initial commit

---
+++ src/main/java/net/hour/aotb/AllOfTheAbove.java
@@ -0,0 +1,17 @@
+package net.hour.aotb;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AllOfTheAbove implements ModInitializer {
+
+	public static final String MOD_ID = "aotb";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/hour/aotb/AllOfTheAboveDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hour.aotb;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AllOfTheAboveDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
